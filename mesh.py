3d60d18379dead34113b8364c85cd66f0a175056
Rottenmanzana <savageking652007@gmail.com>
2023-11-13 18:20:22 -0600

    Creating repository

--- mesh.py
+++ mesh.py
@@ -1,5 +1,6 @@
 from texture import Texture
 from vao import VAO
+# from crosshair import Crosshair
 
 
 class Mesh:
@@ -7,7 +8,9 @@ class Mesh:
         self.app = app
         self.vao = VAO(app.ctx)
         self.texture = Texture(app.ctx)
+        # self.crosshair = Crosshair(app.ctx)
 
     def destroy(self):
         self.vao.destroy()
         self.texture.destroy()
+        self.crosshair.destroy()
